d3465593235ba8fa9b949e559fd9b8feb3965c13
Loc Do <doloc181103@gmail.com>
2023-02-07 23:42:47 +0700

    fix: no cat in main.js #1

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ sdfsdf
 
 sdfsdfsdf
 jsbdfjsdjf
+
+// draw a ascii cat to console
+console.log(```
+    ^__^
+    (oo)\\_______
+    (__)\\       )\\/\\
+        ||----w |
+        ||     ||
+```);
+
